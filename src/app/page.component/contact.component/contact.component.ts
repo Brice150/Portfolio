18e2e01ebd01dd8c03f335042c2969769c1332b7
Brice150 <brice.lecomte0@gmail.com>
2022-08-17 21:46:40 +0200

    prod test of messages

--- src/app/page.component/contact.component/contact.component.ts
+++ src/app/page.component/contact.component/contact.component.ts
@@ -1,8 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class AppComponentContact {}
+export class AppComponentContact {
+  constructor(private http: HttpClient) {}
+
+  onSubmit(contactForm: NgForm) {
+    if (contactForm.valid) {
+      const email = contactForm.value;
+      const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+      this.http.post('https://formspree.io/f/mbjbjwpk',
+        { name: email.name, replyto: email.email, subject: email.subject, message: email.message },
+        { 'headers': headers }).subscribe(
+          (response: any) => {
+            console.log(response);
+            contactForm.reset();
+          }
+        );
+    }
+  }
+}
