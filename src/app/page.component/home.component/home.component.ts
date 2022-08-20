1b346d4a0e687cb8d0b7f997a6aee00c7fa1cb09
Brice150 <brice.lecomte0@gmail.com>
2022-08-20 12:24:19 +0200

    download CV

--- src/app/page.component/home.component/home.component.ts
+++ src/app/page.component/home.component/home.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class AppComponentHome {}
+export class AppComponentHome {
+
+  constructor(private snackBar: MatSnackBar) {}
+
+  download() {
+    this.snackBar.open("CV downloaded", "Dismiss", {duration: 2000});
+  }
+}
