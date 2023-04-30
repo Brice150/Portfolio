dd2c03b2cca9b6df071f47c9b7309c893cfeb803
Brice150 <brice.lecomte0@gmail.com>
2023-04-30 23:17:36 +0200

    new plart project page

--- src/app/plart/plart.module.ts
+++ src/app/plart/plart.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlartComponent } from './plart.component';
 import { RouterModule } from '@angular/router';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   declarations: [PlartComponent],
   imports: [
-    CommonModule, RouterModule
+    CommonModule, RouterModule, MatExpansionModule
   ],
   exports: [PlartComponent]
 })
