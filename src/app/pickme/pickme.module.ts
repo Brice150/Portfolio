cd2c1fb0cb30dc058505b66fdd8e662f1e43a4fe
Brice150 <brice.lecomte0@gmail.com>
2023-04-30 19:27:02 +0200

    pickme project improvements + up to date infos + new toastr

--- src/app/pickme/pickme.module.ts
+++ src/app/pickme/pickme.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PickmeComponent } from './pickme.component';
 import { RouterModule } from '@angular/router';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   declarations: [PickmeComponent],
   imports: [
-    CommonModule, RouterModule
+    CommonModule, RouterModule, MatExpansionModule
   ],
   exports: [PickmeComponent]
 })
