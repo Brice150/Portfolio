b04206eba8fc21fecf847d9e8c44b3d49d476570
brice150 <brice.lecomte0@gmail.com>
2024-02-03 08:43:45 +0100

    delete error path component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
-import { appRouter } from './app.router';
-import { AppComponent } from './app.component';
-import { PageModule } from './page/page.module';
-import { PlartModule } from './projects/plart/plart.module';
-import { ToastrModule } from 'ngx-toastr';
 import { BrowserModule } from '@angular/platform-browser';
-import { DashboardModule } from './projects/dashboard/dashboard.module';
-import { ErrorPathComponent } from './error-path/error-path.component';
+import { ToastrModule } from 'ngx-toastr';
+import { AppComponent } from './app.component';
+import { appRouter } from './app.router';
 import { HomeModule } from './page/home/home.module';
-import { PickmeModule } from './projects/pickme/pickme.module';
+import { PageModule } from './page/page.module';
+import { DashboardModule } from './projects/dashboard/dashboard.module';
 import { GametimeModule } from './projects/gametime/gametime.module';
+import { PickmeModule } from './projects/pickme/pickme.module';
+import { PlartModule } from './projects/plart/plart.module';
 
 @NgModule({
-  declarations: [AppComponent, ErrorPathComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     appRouter,
--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -1,10 +1,9 @@
 import { RouterModule, Routes } from '@angular/router';
 import { PageComponent } from './page/page.component';
-import { PlartComponent } from './projects/plart/plart.component';
 import { DashboardComponent } from './projects/dashboard/dashboard.component';
-import { ErrorPathComponent } from './error-path/error-path.component';
-import { PickmeComponent } from './projects/pickme/pickme.component';
 import { GametimeComponent } from './projects/gametime/gametime.component';
+import { PickmeComponent } from './projects/pickme/pickme.component';
+import { PlartComponent } from './projects/plart/plart.component';
 
 const routes: Routes = [
   { path: '', component: PageComponent },
@@ -12,7 +11,7 @@ const routes: Routes = [
   { path: 'pickme', component: PickmeComponent },
   { path: 'gametime', component: GametimeComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: '**', component: ErrorPathComponent },
+  { path: '**', component: PageComponent },
 ];
 
 export const appRouter = RouterModule.forRoot(routes);
