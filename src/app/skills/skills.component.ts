2c5a2966c2386d2fa5a2f54b60597309077a6df3
brice150 <brice.lecomte0@gmail.com>
2026-01-23 11:44:39 +0100

    skills

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,12 +1,5 @@
 import { CommonModule } from '@angular/common';
-import {
-  Component,
-  ElementRef,
-  inject,
-  Renderer2,
-  signal,
-  viewChild,
-} from '@angular/core';
+import { Component, ElementRef, viewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { environment } from '../../environments/environment';
 
@@ -19,15 +12,6 @@ import { environment } from '../../environments/environment';
 export class SkillsComponent {
   imagePath: string = environment.imagePath + 'skills/';
   readonly scrollContent = viewChild.required<ElementRef>('content');
-  expandedImageSrc = signal<string | null>(null);
-
-  onImageClick(imageSrc: string): void {
-    this.expandedImageSrc.set(imageSrc);
-  }
-
-  closeImage(): void {
-    this.expandedImageSrc.set(null);
-  }
 
   scrollDown(): void {
     this.scrollContent().nativeElement.scrollTo({
