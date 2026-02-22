c11ee9f9f32219c9676b68e9b96c2470e0cfd964
brice150 <brice.lecomte0@gmail.com>
2026-02-22 12:37:41 +0100

    test

---
+++ docs/assets/data/projects.ts
@@ -0,0 +1,30 @@
+import { Project } from '../../app/core/interface/project';
+
+export const projects: Project[] = [
+  {
+    name: 'Gestion vie quotidienne',
+    image: 'projects/LIFE-CONTROL.webp',
+    link: 'https://life-control-c981c.web.app/',
+  },
+  {
+    name: 'Application de rencontre',
+    image: 'projects/PICKME.webp',
+    video: 'PICKME.mp4',
+    link: 'https://github.com/Brice150/PICKME',
+  },
+  {
+    name: 'Quiz multijoueur',
+    image: 'projects/GAMETIME.webp',
+    link: 'https://brice150.github.io/GAMETIME/',
+  },
+  {
+    name: 'Bibliothèque multimédia',
+    image: 'projects/CLASS-E-FILE.webp',
+    link: 'https://brice150.github.io/CLASS-E-FILE/',
+  },
+  {
+    name: 'Simulateur d’aides à la rénovation',
+    image: 'projects/SIMULATOR.webp',
+    link: 'https://brice150.github.io/Simulator/',
+  },
+];
