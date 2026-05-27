16c8d5940c964f98e6c4b94cde3254af51dc9599
Brice150 <brice.lecomte0@gmail.com>
2026-05-27 11:22:09 +0200

    project update

--- src/assets/data/projects.ts
+++ src/assets/data/projects.ts
@@ -7,15 +7,15 @@ export const projects: Project[] = [
     link: 'https://life-rise.web.app/',
   },
   {
+    name: 'Quiz multijoueur',
+    image: 'projects/GAMETIME.webp',
+    link: 'https://brice150.github.io/GAMETIME/',
+  },
+  {
     name: 'Application de rencontre',
     image: 'projects/PICKME.webp',
     video: 'PICKME.mp4',
     link: 'https://github.com/Brice150/PICKME',
-  },
-  {
-    name: 'Quiz multijoueur',
-    image: 'projects/GAMETIME.webp',
-    link: 'https://brice150.github.io/GAMETIME/',
   },
   {
     name: 'Simulateur d’aides à la rénovation',
